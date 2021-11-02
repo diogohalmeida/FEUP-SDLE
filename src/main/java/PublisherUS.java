3d4f740c51793441ce55e4a1059981e05f09c3d8
Diogo Almeida <diogohenriquealmeida@hotmail.com>
2021-11-02 17:32:38 +0000

    Added PUB/SUB weather example

---
+++ src/main/java/PublisherUS.java
@@ -0,0 +1,34 @@
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+import org.zeromq.ZThread;
+
+import java.util.Random;
+
+public class PublisherUS{
+    public static void main(String[] args) throws Exception
+    {
+        //  Prepare our context and publisher
+        try (ZContext context = new ZContext()) {
+            ZMQ.Socket publisher = context.createSocket(SocketType.PUB);
+            publisher.bind("tcp://*:5555");
+            publisher.bind("ipc://weatherUS");
+
+            //  Initialize random number generator
+            Random srandom = new Random(System.currentTimeMillis());
+            while (!Thread.currentThread().isInterrupted()) {
+                //  Get values that will fool the boss
+                int zipcode, temperature, relhumidity;
+                zipcode = 10000 + srandom.nextInt(10000);
+                temperature = srandom.nextInt(215) - 80 + 1;
+                relhumidity = srandom.nextInt(50) + 10 + 1;
+
+                //  Send message to all subscribers
+                String update = String.format(
+                        "%05d %d %d", zipcode, temperature, relhumidity
+                );
+                publisher.send(update, 0);
+            }
+        }
+    }
+}
